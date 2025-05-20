91cb8898b2ca74f33b486209d9b5ff0aefac5e77
Matthew Brisebois <matthew@webheroes.ca>
2025-05-20 07:58:31 -0600

    chore: npm run format

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,92 @@
+export const DELETE = Symbol();
+let debug = false;
 
-export const DELETE			= Symbol();
-let debug				= false;
-
-function log ( msg: any, ...args: any[] ) {
-    let datetime			= (new Date()).toISOString();
-    console.log(`${datetime} [ src/index. ]  INFO: ${msg}`, ...args );
+function log(msg: any, ...args: any[]) {
+    let datetime = new Date().toISOString();
+    console.log(`${datetime} [ src/index. ]  INFO: ${msg}`, ...args);
 }
 
-function run_replacer ( obj: any, key: any, path: any, replacer: any ) {
-    let value				= key === undefined // This must be the top parent element
-	? obj : obj[key];
+function run_replacer(obj: any, key: any, path: any, replacer: any) {
+    let value =
+        key === undefined // This must be the top parent element
+            ? obj
+            : obj[key];
 
-    if ( replacer === undefined )
-	return value; // Because there's nothing to be done
+    if (replacer === undefined) return value; // Because there's nothing to be done
 
-    if ( typeof replacer !== "function" )
-	throw new Error(`Replacer must be a function; not type '${typeof replacer}'`);
+    if (typeof replacer !== 'function')
+        throw new Error(`Replacer must be a function; not type '${typeof replacer}'`);
 
-    if ( key === undefined ) // This must be the top parent element
-	return replacer.call(obj, null, value, path.slice() );
-    else if ( typeof key === "string" || typeof key === "number" )
-	return replacer.call(obj, key, value, path.slice() );
-    else
-	throw new Error(`Unknown key type: ${typeof key}`);
+    if (key === undefined)
+        // This must be the top parent element
+        return replacer.call(obj, null, value, path.slice());
+    else if (typeof key === 'string' || typeof key === 'number')
+        return replacer.call(obj, key, value, path.slice());
+    else throw new Error(`Unknown key type: ${typeof key}`);
 }
 
 // TODO: an option for width first instead of depth first?
-export function walk ( parent: any, replacer: any, key: any, path: any ) {
-    if ( path === undefined )
-	path				= [];
+export function walk(parent: any, replacer: any, key: any, path: any) {
+    if (path === undefined) path = [];
 
-    if ( key !== undefined ) {
-	debug && log(`Extending path (${path.length}) with:`, key, path );
-	path.push( String(key) );
+    if (key !== undefined) {
+        debug && log(`Extending path (${path.length}) with:`, key, path);
+        path.push(String(key));
     }
 
     // If key is undefined than the 'parent' is actually the object we want to start with.
-    let value				= run_replacer( parent, key, path, replacer )
+    let value = run_replacer(parent, key, path, replacer);
 
-    if ( typeof value !== "object" || value === null ) {
-	debug && log("Value is not an object:", value );
-	return value;
+    if (typeof value !== 'object' || value === null) {
+        debug && log('Value is not an object:', value);
+        return value;
     }
 
-    debug && log("Walking depth", path.length, path );
-    if ( Array.isArray(value) ) {
-	debug && log("Walking array with length:", value.length, key );
-	for (let i=0; i < value.length; i++) {
-	    const new_value		= walk( value, replacer, i, path );
+    debug && log('Walking depth', path.length, path);
+    if (Array.isArray(value)) {
+        debug && log('Walking array with length:', value.length, key);
+        for (let i = 0; i < value.length; i++) {
+            const new_value = walk(value, replacer, i, path);
 
-	    if ( new_value === DELETE ) {
-		value.splice(i, 1);
-		i--;
-	    }
-	    else if ( new_value !== value[i] )
-		value[i]		= new_value;
+            if (new_value === DELETE) {
+                value.splice(i, 1);
+                i--;
+            } else if (new_value !== value[i]) value[i] = new_value;
 
-	    debug && log(`Removing path segment (${path.length-1}):`, path[path.length-1] );
-	    path.pop();
-	}
-    }
-    else {
-	debug && log("Walking object:", value );
-	for (let key of Object.keys(value) ) {
-	    debug && log("Walk sub-object for key:", key );
-	    const new_value		= walk( value, replacer, key, path );
+            debug && log(`Removing path segment (${path.length - 1}):`, path[path.length - 1]);
+            path.pop();
+        }
+    } else {
+        debug && log('Walking object:', value);
+        for (let key of Object.keys(value)) {
+            debug && log('Walk sub-object for key:', key);
+            const new_value = walk(value, replacer, key, path);
 
-	    if ( new_value === DELETE )
-		delete value[key];
-	    else if ( new_value !== value[key] )
-		value[key]		= new_value;
+            if (new_value === DELETE) delete value[key];
+            else if (new_value !== value[key]) value[key] = new_value;
 
-	    debug && log(`Removing path segment (${path.length-1}):`, path[path.length-1] );
-	    path.pop();
-	}
+            debug && log(`Removing path segment (${path.length - 1}):`, path[path.length - 1]);
+            path.pop();
+        }
     }
 
     return value;
 }
 
-
 // TODO: add method for gathering nodes as a list instead of using callback (eg Object.nodes)
 
-export function logging () {
-    debug				= true;
+export function logging() {
+    debug = true;
 }
 
-
 export function bindNative() {
-    if ( "walk" in Object )
-	    throw new Error(`Object.walk is already defined as type: ${typeof Object.walk}`);
+    if ('walk' in Object)
+        throw new Error(`Object.walk is already defined as type: ${typeof Object.walk}`);
 
-    Object.defineProperty(Object, "walk", {
-	    "value": walk,
-	    "writable": false,
-	});
+    Object.defineProperty(Object, 'walk', {
+        value: walk,
+        writable: false,
+    });
 
     return base_exports;
 }
@@ -106,5 +98,5 @@ let base_exports = {
 };
 
 export default {
-    ...base_exports
+    ...base_exports,
 };
--- tests/unit/test_walk.js
+++ tests/unit/test_walk.js
@@ -1,115 +1,137 @@
-import path				from 'path';
-import logger				from '@whi/stdlog';
-import { expect }			from 'chai';
-import { logging, DELETE, bindNative }	from '../../lib/index.js';
+import path from 'path';
+import logger from '@whi/stdlog';
+import { expect } from 'chai';
+import { logging, DELETE, bindNative } from '../../lib/index.js';
 
 bindNative();
 
-const log					= logger(path.basename( import.meta.url ), {
+const log = logger(path.basename(import.meta.url), {
     level: process.env.LOG_LEVEL || 'fatal',
 });
 
-if ( process.env.LOG_LEVEL )
-    logging();
+if (process.env.LOG_LEVEL) logging();
 
-
-function createInput () {
-    return [{
-	"fruit": {
-	    "oranges": 10,
-	    "bananas": 4,
-	    "apples": 11,
-	},
-	"candy": [
-	    99,
-	    33,
-	    66,
-	    {
-		"type": "Tank",
-		"name": "Boomer",
-		"bytes": new Uint8Array([1,2,3,4]),
-	    }
-	],
-    }];
+function createInput() {
+    return [
+        {
+            fruit: {
+                oranges: 10,
+                bananas: 4,
+                apples: 11,
+            },
+            candy: [
+                99,
+                33,
+                66,
+                {
+                    type: 'Tank',
+                    name: 'Boomer',
+                    bytes: new Uint8Array([1, 2, 3, 4]),
+                },
+            ],
+        },
+    ];
 }
 
-function basic_tests () {
-    let input				= createInput();
+function basic_tests() {
+    let input = createInput();
 
-    it("should collect all keys", async () => {
-	let keys			= [];
-	let json			= Object.walk( input, function (key, value) {
-	    if ( typeof key === "string" )
-		keys.push( key );
-	    return value;
-	});
+    it('should collect all keys', async () => {
+        let keys = [];
+        let json = Object.walk(input, function (key, value) {
+            if (typeof key === 'string') keys.push(key);
+            return value;
+        });
 
-	expect( keys			).to.deep.equal([ "fruit", "oranges", "bananas", "apples", "candy", "type", "name", "bytes", "0", "1", "2", "3" ]);
+        expect(keys).to.deep.equal([
+            'fruit',
+            'oranges',
+            'bananas',
+            'apples',
+            'candy',
+            'type',
+            'name',
+            'bytes',
+            '0',
+            '1',
+            '2',
+            '3',
+        ]);
     });
 
-    it("should replace Uint8Array", async () => {
-	let keys			= [];
-	let json			= Object.walk( input, function (key, value) {
-	    log.info("Handling key/value: %s => %s", key, typeof value );
-	    if ( typeof key === "string" )
-		keys.push( key );
+    it('should replace Uint8Array', async () => {
+        let keys = [];
+        let json = Object.walk(input, function (key, value) {
+            log.info('Handling key/value: %s => %s', key, typeof value);
+            if (typeof key === 'string') keys.push(key);
 
-	    if ( value instanceof Uint8Array ) {
-		return {
-		    "type": "Uint8Array",
-		    "data": [].slice.call(value),
-		};
-	    }
+            if (value instanceof Uint8Array) {
+                return {
+                    type: 'Uint8Array',
+                    data: [].slice.call(value),
+                };
+            }
 
-	    return value;
-	});
+            return value;
+        });
 
-	expect( keys			).to.deep.equal([ "fruit", "oranges", "bananas", "apples", "candy", "type", "name", "bytes", "type", "data" ]);
+        expect(keys).to.deep.equal([
+            'fruit',
+            'oranges',
+            'bananas',
+            'apples',
+            'candy',
+            'type',
+            'name',
+            'bytes',
+            'type',
+            'data',
+        ]);
     });
 
-    it("should replace base", async () => {
-	let text			= Object.walk( Buffer.from("Hello"), function (key, value) {
-	    let hexstr			= value.toString('hex').match(/.{2}/g).join(" ");
-	    return `<Buffer ${hexstr}>`;
-	});
+    it('should replace base', async () => {
+        let text = Object.walk(Buffer.from('Hello'), function (key, value) {
+            let hexstr = value.toString('hex').match(/.{2}/g).join(' ');
+            return `<Buffer ${hexstr}>`;
+        });
 
-	expect( text			).to.equal("<Buffer 48 65 6c 6c 6f>");
+        expect(text).to.equal('<Buffer 48 65 6c 6c 6f>');
     });
 
-    it("should collect all paths", async () => {
-	let paths			= [];
-	let json			= Object.walk( input, function (key, value, path) {
-	    paths.push( path.join(".") );
-	    return value;
-	});
+    it('should collect all paths', async () => {
+        let paths = [];
+        let json = Object.walk(input, function (key, value, path) {
+            paths.push(path.join('.'));
+            return value;
+        });
 
-	expect( paths			).to.have.length( 20 );
-	expect( paths.pop()		).to.equal("0.candy.3.bytes.data.3");
+        expect(paths).to.have.length(20);
+        expect(paths.pop()).to.equal('0.candy.3.bytes.data.3');
     });
 
-    it("should remove ArrayBuffer views", async () => {
-	input				= createInput();
-	let result			= Object.walk( input, function (key, value) {
-	    return ArrayBuffer.isView( value ) ? DELETE : value;
-	});
+    it('should remove ArrayBuffer views', async () => {
+        input = createInput();
+        let result = Object.walk(input, function (key, value) {
+            return ArrayBuffer.isView(value) ? DELETE : value;
+        });
 
-	expect( JSON.stringify(result)	).to.equal(`[{"fruit":{"oranges":10,"bananas":4,"apples":11},"candy":[99,33,66,{"type":"Tank","name":"Boomer"}]}]`);
+        expect(JSON.stringify(result)).to.equal(
+            `[{"fruit":{"oranges":10,"bananas":4,"apples":11},"candy":[99,33,66,{"type":"Tank","name":"Boomer"}]}]`
+        );
     });
 
-    it("should do nothing on an undefined value", async () => {
-	let input			= {
-	    "something": undefined,
-	};
-	let result			= Object.walk( input, function (key, value) {
-	    return value;
-	});
+    it('should do nothing on an undefined value', async () => {
+        let input = {
+            something: undefined,
+        };
+        let result = Object.walk(input, function (key, value) {
+            return value;
+        });
 
-	expect( JSON.stringify(result)	).to.equal(`{}`);
+        expect(JSON.stringify(result)).to.equal(`{}`);
     });
 }
 
-describe("Debug", () => {
-
-    describe("Basic", basic_tests );
-
+describe('Debug', () => {
+    describe('Basic', basic_tests);
 });
